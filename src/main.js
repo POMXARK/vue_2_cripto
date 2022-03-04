07cbfb5f8c05a4886e6808fdbc27292e218b7676
POMXARK <1997pom@gmail.com>
2022-03-04 20:36:37 +1000

    master

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import TestTraid from './TestTraid.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+new Vue({
+  render: h => h(TestTraid),
+}).$mount('#test_traid')
